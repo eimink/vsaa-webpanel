156861bf6dea10827e604ef074227555a36118d3
Teppo Kauppinen <eimink@gmail.com>
2014-07-26 22:12:01 +0300

    Using VSAA-style config

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 // server.js
 
-// set up ======================================================================
-// get all the tools we need
+
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 8080;
 
 var passport = require('passport');
 var flash    = require('connect-flash');
 
-// configuration ===============================================================
-// connect to our database
+global.config = require('./config/config');
+global.db = require("./app/databases/"+config.db_driver);
 
 require('./config/passport')(passport); // pass passport for configuration
 
@@ -24,7 +22,7 @@ app.configure(function() {
 	app.set('view engine', 'ejs'); // set up ejs for templating
 
 	// required for passport
-	app.use(express.session({ secret: 'vidyapathaisalwaysrunning' } )); // session secret
+	app.use(express.session({ secret: 'verysimpleappanalyticsftw' } )); // session secret
 	app.use(passport.initialize());
 	app.use(passport.session()); // persistent login sessions
 	app.use(flash()); // use connect-flash for flash messages stored in session
@@ -35,5 +33,5 @@ app.configure(function() {
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+app.listen(config.listen);
+console.log('vsaa-webpanel running on port ' + config.listen);
