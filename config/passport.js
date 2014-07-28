f705e132153d37364163034a2be7726d75b6a419
Teppo Kauppinen <eimink@gmail.com>
2014-07-28 22:13:49 +0300

    Added Facebook authentication strategy

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@
 				
 // load all the things we need
 var LocalStrategy   = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var bcrypt = require('bcrypt-nodejs'); // we will use this to salt and encrypt our passwords
 var validator = require('validator');
 var emailExistence = require('email-existence');
@@ -107,6 +108,63 @@ module.exports = function(passport) {
 		
 		});
 		
+    }));
+    
+    // =========================================================================
+    // FACEBOOK ================================================================
+    // =========================================================================
+    passport.use(new FacebookStrategy({
+
+		// pull in our app id and secret from our auth.js file
+        clientID        : configAuth.facebookAuth.clientID,
+        clientSecret    : configAuth.facebookAuth.clientSecret,
+        callbackURL     : configAuth.facebookAuth.callbackURL
+
+    },
+
+    // facebook will send back the token and profile
+    function(token, refreshToken, profile, done) {
+
+		// asynchronous
+		process.nextTick(function() {
+
+			// find the user in the database based on their facebook id
+			db.getUserByFB(profile.id ,function(err,rows){
+
+	        	// if there is an error, stop everything and return that
+	        	// ie an error connecting to the database
+	            if (err)
+	                return done(err);
+
+				// if the user is found, then log them in
+	            if (rows.length) {
+	                return done(null, user); // user found, return that user
+	            } else {
+	                // if there is no user found with that facebook id, create them
+	                var newUser            = new User();
+					var salt = bcrypt.genSaltSync(10);
+					// set all of the facebook information in our user model
+	                newUser.facebook.id    = profile.id; // set the users facebook id	                
+	                newUser.facebook.token = token; // we will save the token that facebook provides to the user	                
+	                newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+	                newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+					newUser.email    = email;
+					newUser.password = '1'+bcrypt.hashSync(token, salt);
+					newUser.salt = salt;
+					// save our user to the database
+	                db.createUser(newUser,function(err,res){
+						if(err){
+							console.log(err);
+							return done(null, false);
+						}
+						newUser.id = res.insertId;
+						return done(null, newUserMysql);
+					});
+	            }
+
+	        });
+        });
+
     }));
 
 };
